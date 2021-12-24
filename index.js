94c8d75b72e774eaa21eb2bd0819d8576a4860e1
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2021-12-25 03:38:12 +0600

    lets learn concepts of socket io rooms

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+require('dotenv').config();
+
+const port = process.env.PORT || 4040
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+
+
+
+});
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
